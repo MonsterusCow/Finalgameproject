e11df6d8335d3961defe3670d9950cdd9fb74c66
MonsterusCow <ryan@starkfamily.org>
2024-04-25 11:30:14 -0500

    SPIN NO WORK

--- src/RoulettePanel.java
+++ src/RoulettePanel.java
@@ -10,6 +10,9 @@ public class RoulettePanel extends JPanel implements ImageObserver {
 
     BufferedImage R0,R1,R2,R3,R4,R5,R6,R7,R8,R9,R10,R11,R12,R13,R14,R15;
     BufferedImage[] symbols = {R0,R1,R2,R3,R4,R5,R6,R7,R8,R9,R10,R11,R12,R13,R14,R15};
+    int draw = 0;
+    static boolean spin = false;
+    TextBox store;
 
     public RoulettePanel() {
         setBackground(new Color(55, 208, 30));
@@ -24,25 +27,24 @@ public class RoulettePanel extends JPanel implements ImageObserver {
         roulette.drawImgBox(this);
 
         if (RouletteMouseListener.clicked) {
-            try {R0 = ImageIO.read(new File("Images/Routlette/R0.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R1 = ImageIO.read(new File("Images/Routlette/R1.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R2 = ImageIO.read(new File("Images/Routlette/R2.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R3 = ImageIO.read(new File("Images/Routlette/R3.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R4 = ImageIO.read(new File("Images/Routlette/R4.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R5 = ImageIO.read(new File("Images/Routlette/R5.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R6 = ImageIO.read(new File("Images/Routlette/R6png"));} catch (IOException e) {e.printStackTrace();}
-            try {R7 = ImageIO.read(new File("Images/Routlette/R7.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R8 = ImageIO.read(new File("Images/Routlette/R8.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R9 = ImageIO.read(new File("Images/Routlette/R9.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R10 = ImageIO.read(new File("Images/Routlette/R10.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R11 = ImageIO.read(new File("Images/Routlette/R11.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R12 = ImageIO.read(new File("Images/Routlette/R12.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R13 = ImageIO.read(new File("Images/Routlette/R13.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R14 = ImageIO.read(new File("Images/Routlette/R14.png"));} catch (IOException e) {e.printStackTrace();}
-            try {R15 = ImageIO.read(new File("Images/Routlette/R15.png"));} catch (IOException e) {e.printStackTrace();}
-            for (int i = 0; i < 16; i++) {
-                roulette = new TextBox(UpgradePanel.Ximg, "Images/Routlette/R"+i+".png", new int[] {Main.rouletteFrame.getWidth()/2-200, Main.rouletteFrame.getWidth()/2+200, Main.rouletteFrame.getWidth()/2+200, Main.rouletteFrame.getWidth()/2-200}, new int[] {Main.rouletteFrame.getHeight()/2-200, Main.rouletteFrame.getHeight()/2-200, Main.rouletteFrame.getHeight()/2+200, Main.rouletteFrame.getHeight()/2+200}, g);
-                roulette.drawImgBox(this);
+            if (roulette.Clicked(RouletteMouseListener.clickedx, RouletteMouseListener.clickedy)){
+                spin = true;
+            }
+        }
+
+        if (spin){
+            if (draw%4==0) {
+                roulette = new TextBox(UpgradePanel.Ximg, "Images/Routlette/R" + (draw/4) + ".png", new int[]{Main.rouletteFrame.getWidth() / 2 - 200, Main.rouletteFrame.getWidth() / 2 + 200, Main.rouletteFrame.getWidth() / 2 + 200, Main.rouletteFrame.getWidth() / 2 - 200}, new int[]{Main.rouletteFrame.getHeight() / 2 - 200, Main.rouletteFrame.getHeight() / 2 - 200, Main.rouletteFrame.getHeight() / 2 + 200, Main.rouletteFrame.getHeight() / 2 + 200}, g);
+                store = roulette;
+            } else {
+                roulette = store;
+
+            roulette.drawImgBox(this);
+            if (draw < 64) {
+                draw++;
+            } else {
+                draw = 0;
+                spin = false;
             }
         }
 
