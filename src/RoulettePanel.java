7a8d64f277a8b0ca86dbe996253e59004938186f
MonsterusCow <ryan@starkfamily.org>
2024-04-26 10:26:35 -0500

    disapear after spin

--- src/RoulettePanel.java
+++ src/RoulettePanel.java
@@ -10,9 +10,11 @@ public class RoulettePanel extends JPanel implements ImageObserver {
 
     BufferedImage R0,R1,R2,R3,R4,R5,R6,R7,R8,R9,R10,R11,R12,R13,R14,R15;
     BufferedImage[] symbols = {R0,R1,R2,R3,R4,R5,R6,R7,R8,R9,R10,R11,R12,R13,R14,R15};
-    int draw = 0;
+    static int draw = 0;
+    static int ticks = 64;
+    static boolean dra = true;
     static boolean spin = false;
-    TextBox store;
+    static TextBox store, roulette;
 
     public RoulettePanel() {
         setBackground(new Color(55, 208, 30));
@@ -22,30 +24,35 @@ public class RoulettePanel extends JPanel implements ImageObserver {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-
-        TextBox roulette = new TextBox(UpgradePanel.Ximg, "Images/Routlette/R0.png", new int[] {Main.rouletteFrame.getWidth()/2-200, Main.rouletteFrame.getWidth()/2+200, Main.rouletteFrame.getWidth()/2+200, Main.rouletteFrame.getWidth()/2-200}, new int[] {Main.rouletteFrame.getHeight()/2-200, Main.rouletteFrame.getHeight()/2-200, Main.rouletteFrame.getHeight()/2+200, Main.rouletteFrame.getHeight()/2+200}, g);
-        roulette.drawImgBox(this);
+        if (dra) {
+            roulette = new TextBox(UpgradePanel.Ximg, "Images/Routlette/R0.png", new int[]{Main.rouletteFrame.getWidth() / 2 - 200, Main.rouletteFrame.getWidth() / 2 + 200, Main.rouletteFrame.getWidth() / 2 + 200, Main.rouletteFrame.getWidth() / 2 - 200}, new int[]{Main.rouletteFrame.getHeight() / 2 - 200, Main.rouletteFrame.getHeight() / 2 - 200, Main.rouletteFrame.getHeight() / 2 + 200, Main.rouletteFrame.getHeight() / 2 + 200}, g);
+            roulette.drawImgBox(this);
+        }
 
         if (RouletteMouseListener.clicked) {
             if (roulette.Clicked(RouletteMouseListener.clickedx, RouletteMouseListener.clickedy)){
                 spin = true;
+                System.out.println(spin +"spin");
             }
         }
 
-        if (spin){
-            if (draw%4==0) {
-                roulette = new TextBox(UpgradePanel.Ximg, "Images/Routlette/R" + (draw/4) + ".png", new int[]{Main.rouletteFrame.getWidth() / 2 - 200, Main.rouletteFrame.getWidth() / 2 + 200, Main.rouletteFrame.getWidth() / 2 + 200, Main.rouletteFrame.getWidth() / 2 - 200}, new int[]{Main.rouletteFrame.getHeight() / 2 - 200, Main.rouletteFrame.getHeight() / 2 - 200, Main.rouletteFrame.getHeight() / 2 + 200, Main.rouletteFrame.getHeight() / 2 + 200}, g);
+        if (spin) {
+            dra = false;
+            if (ticks >= (0+((int)(Math.random()*10)-5))) {
+                roulette = new TextBox(UpgradePanel.Ximg, "Images/Routlette/R" + (draw) + ".png", new int[]{Main.rouletteFrame.getWidth() / 2 - 200, Main.rouletteFrame.getWidth() / 2 + 200, Main.rouletteFrame.getWidth() / 2 + 200, Main.rouletteFrame.getWidth() / 2 - 200}, new int[]{Main.rouletteFrame.getHeight() / 2 - 200, Main.rouletteFrame.getHeight() / 2 - 200, Main.rouletteFrame.getHeight() / 2 + 200, Main.rouletteFrame.getHeight() / 2 + 200}, g);
+                roulette.drawImgBox(this);
                 store = roulette;
+                if (draw < 15) {
+                    draw++;
+                    ticks--;
+                } else {
+                    draw = 0;
+                }
             } else {
-                roulette = store;
-
-            roulette.drawImgBox(this);
-            if (draw < 64) {
-                draw++;
-            } else {
-                draw = 0;
                 spin = false;
             }
+        } else {
+            roulette = store;
         }
 
         try {
